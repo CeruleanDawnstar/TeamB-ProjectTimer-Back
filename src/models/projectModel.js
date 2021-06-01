bfb895b59fbaeae6ca8169ac288b284176b39a79
Chris Bahonda <chrisbahonda@gmail.com>
2021-06-01 16:43:07 +0200

    Add model for a project

---
+++ src/models/projectModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const projectSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true
+		},
+		description: {
+			type: String,
+			required: true
+		},
+		assignedTeams: {
+			type: Array,
+			default: []
+		},
+		tasks: {
+			type: Array,
+			default: []
+		},
+		createdBy: {
+			type: ObjectId,
+			ref: 'User'
+		}
+	},
+);
+
+module.exports = mongoose.model('Project', projectSchema);
