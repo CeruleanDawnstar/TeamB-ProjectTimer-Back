c98b739a4e71bd468f996c57a7dff6c054add410
antoine <nogoup.antoine@gmail.com>
2021-06-02 18:05:10 +0200

    crud group first commit

---
+++ src/models/teamModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const timestamps = require('mongoose-timestamps')
+
+const teamSchema = new Schema(
+	{
+		name: {
+			type: String
+		},
+		createdBy: {
+			type: String
+		},
+		description: {
+			type: String,
+			required: true
+		},
+		id_project: {
+			type: String,
+
+		}
+	}
+);
+
+teamSchema.plugin(timestamps);
+
+module.exports = mongoose.model('Team', teamSchema);
