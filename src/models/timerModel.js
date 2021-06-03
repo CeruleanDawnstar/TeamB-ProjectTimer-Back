c8984e613b0231265c346d4a00c5b4e58829a2e8
Chris Bahonda <chrisbahonda@gmail.com>
2021-06-03 09:57:56 +0200

    Add a model for timer

---
+++ src/models/timerModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const timerSchema = mongoose.Schema(
+	{
+            loggedTime: {
+                type: Number,
+                required: true,
+                default: 0
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            description: {
+                type: String
+            },
+            project: {
+                type: ObjectId,
+                ref: 'Project'
+            }
+
+	},
+);
+
+module.exports = mongoose.model('Timer', timerSchema);
