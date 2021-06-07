a5979f319c06e2ef5692fd46c2fb4ab7826b708e
Chris Bahonda <chrisbahonda@gmail.com>
2021-06-07 11:24:09 +0200

    Add new routes for projects

---
+++ src/routes/projectRoute.js
@@ -0,0 +1,15 @@
+const projectController = require('../controllers/projectController');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+
+module.exports = (server) => {
+    server.route('/project')
+        .post(jwtMiddleware.verifyToken, projectController.createProject);
+
+    server.route('/project/:id')
+        .get(jwtMiddleware.verifyToken, projectController.getProjectById)
+        .put(jwtMiddleware.verifyToken, projectController.updateProject)
+        .delete(jwtMiddleware.verifyToken, projectController.deleteProject)
+        
+    server.route('/projects/')
+        .get(jwtMiddleware.verifyToken, projectController.getAllProjects)
+}
