405e1670d169a607c96a35ead887ecf2903c20ee
Chris Bahonda <chrisbahonda@gmail.com>
2021-06-15 13:43:33 +0200

    Add route for Users

---
+++ src/routes/userRoute.js
@@ -0,0 +1,21 @@
+const userController = require('../controllers/userController');
+const jwtMiddleware = require('../middlewares/jwtmiddleware');
+
+module.exports = (server) => {
+    server.post('/user/login', userController.userLogin);
+    server.post('/user/register', userController.userRegister);
+
+    server.route('/users')
+        .get(jwtMiddleware.verifyToken, userController.getAllUsers)
+
+    server.route('/user')
+        .put(jwtMiddleware.verifyToken, userController.updateUser)
+        .delete(jwtMiddleware.verifyToken, userController.deleteUser)
+
+    server.route('/user/:id')
+        .get(jwtMiddleware.verifyToken, userController.getUserById)
+
+    server.route('/logout')
+        .get(jwtMiddleware.verifyToken, userController.logout)
+
+}
