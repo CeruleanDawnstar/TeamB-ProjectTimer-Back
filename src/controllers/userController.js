34111ac0c42a9b66a73b9288c1b1fdbca72004cf
Chris Bahonda <chrisbahonda@gmail.com>
2021-06-15 13:42:58 +0200

    Add methods to userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/userModel');
+const mongoose = require('mongoose');
+const Model = mongoose.model("User");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
@@ -86,3 +88,85 @@ exports.userLogin = (req, res) => {
         }
     });
 }
+
+exports.getAllUsers = async (req, res) => {
+    try{
+        Model.find({}, (error, user) => {
+            if (error) {
+                console.log(error);
+            } else{
+                res.json(200);
+                res.json(user);
+            }
+        })
+
+    } catch (err){
+        console.log(err);
+    }
+}
+
+exports.getUserById = async (req, res) => {
+    try{
+        const user = req.params.id;
+
+        await Model.findById({_id:user}, (err, res) => {
+            if (err){
+                console.log(err)
+            } else{
+                res.status(200);
+                res.json(res)
+            }
+        })
+    } catch (error){
+        console.log(error)
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try{
+        const find = {
+            _id: req.user._id
+        }
+
+        const update = {
+            username: req.body.username,
+            email: req.body.email
+        }
+
+        Model.findOneAndUpdate(find, update, {new : true}, (error, updated) => {
+            if (error){
+                console.log(error)
+            } else{
+                res.status(200);
+                res.json(updated)
+            }
+        })
+    } catch(err){
+        console.log(err)
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try{
+        const find = {
+            _id: req.user._id
+        }
+
+        Model.remove(find, (error) => {
+            if (error){
+                console.log(error)
+            } else{
+                res.status(200);
+                res.json({"message" : "The user was deleted"})
+            }
+        })
+    } catch(err){
+        console.log(err)
+    }
+}
+
+exports.logout = (req, res) => {
+    res.status(200);
+    res.json({"message" : "Logout successful"})
+}
+
